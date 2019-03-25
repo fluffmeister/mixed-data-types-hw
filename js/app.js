45528e9b593feaf49a6d09bcce94fa1a6fde764f
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-25 15:52:59 -0700

    created HW

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+//1. Data types
+// A light switch that can be either on or off.
+boolean
+// A user's email address.
+string
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+objects
+// A list of student names from our class.
+array,object
+// A list of student names from our class, each with a location.
+array,object
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+array,object
+*/
+//2. Take it Easy
+// Make an array that holds all of the colors of the rainbow.
+colors=['red','orange','yellow','green','blue','indigo','violet']
+console.log(colors)
+// Write code that will access "blue" from the rainbow array.
+colors[4]
+// Make an object that is called your name and holds the information about your favorite food, a hobby, 
+//the name of the town that you live in currently, and your favorite datatype.
+
+// Write code that will access your hobby from the object that you just created.
